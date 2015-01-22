6dc35eec292c5ec55800590bac1d79ef41c3eb58
Dallas Reedy <code@dallasreedy.com>
2015-01-22 16:57:05 -0700

    allow either double- or single-quoted task names

--- gulp-autocompletion.zsh
+++ gulp-autocompletion.zsh
@@ -20,7 +20,7 @@
 # `gulpfile.coffee` in the current directory.
 #
 function $$gulp_completion() {
-    compls=$(grep -Eo "gulp.task\(? *('(([a-zA-Z0-9]|-)*)',)" gulpfile.{js,coffee} 2>/dev/null | grep -Eo "'(([a-zA-Z0-9]|-)*)'" | sed s/"'"//g | sort)
+    compls=$(grep -Eo "gulp.task\(? *([\"'](([a-zA-Z0-9]|-)*)[\"'],)" gulpfile.{js,coffee} 2>/dev/null | grep -Eo "[\"'](([a-zA-Z0-9]|-)*)[\"']" | sed s/"'"//g | sort)
 
     completions=(${=compls})
     compadd -- $completions
